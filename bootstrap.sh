0cb57c64fb315481f894b0d1495dbe26000db81d
avguston <avguston@gmail.com>
2021-05-13 22:23:51 +0200

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 AAA='https://github.com/fliper-b3/aaa'
-[ -d ${HOME}/aaa ] && cd ${HOME}/aaa && git pull
-[ ! -d ${HOME}/aaa ] && /usr/bin/git clone ${AAA} ${HOEM}/aaa || (echo "Can't download main repo ${AAA}" && echo 1 )
+if [ -d ${HOME}/aaa ]; then 
+   pushd ${HOME}/aaa && git pull
+   popd > /dev/null
+else
+   /usr/bin/git clone ${AAA} ${HOEM}/aaa && echo "Can't download main repo ${AAA}" && echo 1
+fi
 /usr/bin/python3.9 --version > /dev/null || ( echo "/usr/bin/python3.9 not found" && exit 1 )
 /usr/bin/python3.9 -m venv ${HOEM}/aaa/.venv
 ${HOME}/aaa/.venv/pip install ansible
