c7278e15790dab0fc9e2b264d433a26bde3bc2d2
avguston <avguston@gmail.com>
2021-05-13 22:25:16 +0200

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ else
    /usr/bin/git clone ${AAA} ${HOEM}/aaa && echo "Can't download main repo ${AAA}" && echo 1
 fi
 /usr/bin/python3.9 --version > /dev/null || ( echo "/usr/bin/python3.9 not found" && exit 1 )
-/usr/bin/python3.9 -m venv ${HOEM}/aaa/.venv
+/usr/bin/python3.9 -m venv ${HOME}/aaa/.venv
 ${HOME}/aaa/.venv/pip install ansible
 ${HOME}/.venv/bin/ansible-playbook -c local -i 127.0.0.1, --extra-vars=user_name=ivan ${HOME}/aaa/soft.yml
