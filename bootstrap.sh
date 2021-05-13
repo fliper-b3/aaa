ce1ef577cfe54c9c39cff1338b7f39ea26eb531d
avguston <avguston@gmail.com>
2021-05-14 00:03:23 +0200

    final steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 AAA='https://github.com/fliper-b3/aaa'
 if [ -d ${HOME}/aaa ]; then 
-   pushd ${HOME}/aaa && git pull
+   pushd ${HOME}/aaa
+   /usr/bin/git pull
    [ $? -ne 0 ] && echo "Can't pull repo ${AAA}" && exit 1
    popd > /dev/null
 else
@@ -11,4 +12,4 @@ fi
 /usr/bin/python3.9 --version > /dev/null || ( echo "/usr/bin/python3.9 not found" && exit 1 )
 /usr/bin/python3.9 -m venv ${HOME}/aaa/.venv
 ${HOME}/aaa/.venv/bin/pip install ansible
-${HOME}/.venv/bin/ansible-playbook -c local -i 127.0.0.1, --extra-vars=user_name=ivan ${HOME}/aaa/soft.yml
+${HOME}/aaa/.venv/bin/ansible-playbook -c local -i 127.0.0.1, --extra-vars=user_name=ivan ${HOME}/aaa/soft.yml
