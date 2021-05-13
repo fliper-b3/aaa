d0e18791cc252ed5b4fca4e5b3bfec947a8ed983
avguston <avguston@gmail.com>
2021-05-13 19:56:42 +0200

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-cd $(dirname $0)
-/usr/bin/python3.9 -m venv .venv
-/usr/bin/source .venv/bin/activate
-pip install ansible
-${HOME}/.venv/bin/ansible-playbook -c local -i 127.0.0.1, --extra-vars=user_name=ivan soft.yml
+AAA='https://github.com/fliper-b3/aaa'
+/usr/bin/git clone ${AAA} ${HOEM}/aaa
+[ $? -ne 0 ] && echo "Can't download main repo ${AAA}" && echo 1
+/usr/bin/python3.9 --version > echo "/usr/bin/python3.9 not found" && exit 1
+/usr/bin/python3.9 -m venv ${HOEM}/aaa/.venv
+${HOME}/aaa/.venv/pip install ansible
+${HOME}/.venv/bin/ansible-playbook -c local -i 127.0.0.1, --extra-vars=user_name=ivan ${HOME}/aaa/soft.yml
